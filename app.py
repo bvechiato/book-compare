3281ba2069578ba26673ff313427a44e2ce450fc
biavechiato <bia.vechiato@gmail.com>
2021-12-12 18:47:18 +0000

    Update app.py
    more modular, selenium stuff's now in checks.py

--- app.py
+++ app.py
@@ -1,16 +1,7 @@
-from flask import Flask, json, render_template, request
-from flask_sqlalchemy import SQLAlchemy
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support import expected_conditions as EC
-from datetime import datetime
-import random, string
+from flask import Flask, render_template, request
+import checks
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
-db = SQLAlchemy(app)
   
 @app.route('/', methods=['POST', 'GET'])
 def index():
@@ -18,28 +9,14 @@ def index():
         bookName = request.form['bookName']
         bookISBN = request.form['bookISBN']
 
-        # selenium
-        driver = webdriver.Chrome()
+        if bookISBN == "":
+            bookISBN = checks.findISBN(bookName)
 
-        if bookISBN == None:
-            driver.get("isbnsearch.org")
-            searchBar = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.ID, "searchQuery")))
-            searchBar.send_keys(bookName)
-            searchBar.send_keys(Keys.RETURN)
-            firstSearchResult = driver.find_element(By.XPATH, "//*[@id='searchresults']/li[1]/div[2]/h2/a") 
-            foundISBN = firstSearchResult.get_attribute('href')[6:]
-            bookISBN = foundISBN
+        price_waterstones = checks.waterstones(bookISBN)
+        price_wob = checks.wob(bookISBN)
+        price_amazon = checks.amazon(bookISBN)
 
-        ## waterstones
-        
-
-        elements = WebDriverWait(driver, 20).until(EC.visibility_of_all_elements_located((By.CLASS_NAME, "content")))
-
-        # isbnsearch.org
-        # amazon
-        # world of books
-        # waterstones - https://www.waterstones.com/books/search/term/[ISBN]
-        return render_template('main.html')
+        return render_template('main.html', wob=price_wob, waterstones=price_waterstones, amazon=price_amazon)
     else: 
         return render_template('base.html')
     
