1f3b1cf09ed1254e3f41cf1ee9be28d0090bad71
beatriz <44908994+bvechiato@users.noreply.github.com>
2022-06-14 17:03:20 +0100

    Dynamic urls

--- app/cache_manager.py
+++ app/cache_manager.py
@@ -3,18 +3,39 @@ from app.models import Book
 
 r = redis.Redis(host='localhost', port=6379)
 
-def check(key) -> bool:
+def check(key: str) -> bool:
+    """Takes a key and checks if it exists in cache
+
+    Args:
+        key (str): isbn
+
+    Returns:
+        bool: exists in cache or not
+    """
     return r.exists(key)
 
-def set(key, value: Book):
+def set(key: str, value: Book):
     value = str(value)
     r.setex(key, 172800, value)
 
-def get(key) -> list[str]:
+def get(key: str) -> list[str]:
+    """Takes the key and returns the book object in a string
+
+    Args:
+        key (str): isbn
+
+    Returns:
+        list[str]: book object in a string
+    """
     decoded = r.get(key).decode('UTF-8')
     return decoded.split(", ")
 
 def get_all() -> list[list[str]]:
+    """Gets all keys and values stored in cache
+
+    Returns:
+        list[list[str]]: 2D list of all books stored in cache as strings in the format [[isbn, title, url_goodreads, price_wob, price_waterstones, price_blackwells, url_wob, url_waterstones, url_blackwells], [...]]
+    """
     # get keys
     keys = r.keys()
     
--- app/routes.py
+++ app/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template, request
+from flask import render_template, request, redirect
 from checks import checks_with_bs4, checks_with_selenium
 from app import app, cache_manager
 from app import models
@@ -6,28 +6,36 @@ from app import models
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
+    recently_searched = cache_manager.get_all()
     if request.method == 'POST':
         bookISBN = request.form['bookISBN']
 
         # if isbn isn't entered
         if bookISBN == "":
             bookISBN = checks_with_bs4.goodreads_with_bookName(request.form['bookName'])
-        book_info = checks_with_bs4.goodreads_with_ISBN(bookISBN)
-        
-        # check if book already exists in cache
-        if cache_manager.check(bookISBN):
-            display_book = cache_manager.get(bookISBN)
-            return render_template('main.html', book=display_book)
-
-        waterstones = checks_with_bs4.waterstones(bookISBN)
-        wob = checks_with_bs4.wob(bookISBN)
-        blackwells = checks_with_bs4.blackwells(bookISBN)
-        
-        new_book = models.Book(bookISBN, book_info[0], book_info[1], wob[0], waterstones[0], blackwells[0], wob[1], waterstones[1], blackwells[1])
-        cache_manager.set(bookISBN, new_book)
-        display_book = str(new_book).split(", ")
-            
-        return render_template('main.html', book=display_book)
+        return redirect(f'/{bookISBN}')
     else: 
-        recently_searched = cache_manager.get_all()
         return render_template('base.html', recently_searched=recently_searched)
+    
+@app.route('/<int:bookISBN>', methods=['POST', 'GET'])
+def search(bookISBN):
+    recently_searched = cache_manager.get_all()
+    bookISBN = str(bookISBN)
+    book_info = checks_with_bs4.goodreads_with_ISBN(bookISBN)
+        
+    # check if book already exists in cache
+    if cache_manager.check(bookISBN):
+        display_book = cache_manager.get(bookISBN)
+        return render_template('main.html', book=display_book, recently_searched=recently_searched)
+
+    # get price
+    waterstones = checks_with_bs4.waterstones(bookISBN)
+    wob = checks_with_bs4.wob(bookISBN)
+    blackwells = checks_with_bs4.blackwells(bookISBN)
+        
+    new_book = models.Book(bookISBN, book_info[0], book_info[1], wob[0], waterstones[0], blackwells[0], wob[1], waterstones[1], blackwells[1])
+    cache_manager.set(bookISBN, new_book)
+    display_book = str(new_book).split(", ")
+        
+    recently_searched = cache_manager.get_all()
+    return render_template('main.html', book=display_book, recently_searched=recently_searched)
