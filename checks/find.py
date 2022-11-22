df53c89d5abfc01080744c0184789b4833ab8dba
bvechiato <beaaccounts@icloud.com>
2022-11-22 23:47:30 +0000

    ISBN Search

--- checks/find.py
+++ checks/find.py
@@ -1,54 +1,90 @@
 from bs4 import BeautifulSoup
 from lxml import etree
-import requests
 import cloudscraper
+
+
+# def title(search_term: str) -> list[str]:
+#     """Scrapes the search result and returns the first's title
+#
+#     Args:
+#         search_term (str): input by the user
+#
+#     Returns:
+#         str: title
+#     """
+#     scraper = cloudscraper.create_scraper()
+#     goodreadsURL = "https://www.goodreads.com/search?q=" + search_term.replace(' ', '+')
+#     print("goodreadsURL: " + goodreadsURL)
+#
+#     data = scraper.get(goodreadsURL).content
+#     soup = BeautifulSoup(data, 'lxml')
+#
+#     dom = etree.HTML(str(soup))
+#
+#     # Find title
+#     try:
+#         # If we're taken to search page
+#         found_title = dom.xpath("//table/tr[1]/td[1]/a/@title")[0]
+#     except IndexError:
+#         # if we're taken to the product page
+#         try:
+#             found_title = dom.xpath("//*[@id='bookTitle']")[0]
+#             found_title = found_title.text
+#         except IndexError:
+#             return ["book unavailable", "https://www.goodreads.com", ""]
+#
+#     # Find url
+#     try:
+#         # If we're taken to the search page
+#         url = dom.xpath("//table/tr[1]/td[1]/a/@href")[0]
+#         url = "https://www.goodreads.com" + url
+#     except IndexError:
+#         # If we're taken to the product page
+#         try:
+#             url = goodreadsURL
+#         except IndexError:
+#             return [found_title, "https://www.goodreads.com", ""]
+#
+#     # Find author
+#     try:
+#         # If we're taken to the search or product page
+#         author = dom.xpath("//*[@class='authorName']/span")[0]
+#     except IndexError:
+#         return [found_title, url, ""]
+#     return [found_title, url, author.text]
+
 
 def title(search_term: str) -> list[str]:
     """Scrapes the search result and returns the first's title
 
-    Args:
-        book_title (str): inputted by the user
+        Args:
+            search_term (str): input by the user
 
-    Returns:
-        str: title
-    """
+        Returns:
+            str: title
+        """
     scraper = cloudscraper.create_scraper()
-    goodreadsURL = "https://www.goodreads.com/search?q=" + search_term.replace(' ', '+') 
-    print("goodreadsURL: " + goodreadsURL)
+    isbn_search_url = "https://isbnsearch.org/search?s=" + search_term.replace(' ', '+')
+    print("isbn_search_url: " + isbn_search_url)
 
-    data = scraper.get(goodreadsURL).content
+    data = scraper.get(isbn_search_url).content
     soup = BeautifulSoup(data, 'lxml')
-    
+
     dom = etree.HTML(str(soup))
-    
+
     # Find title
     try:
         # If we're taken to search page
-        title = dom.xpath("//table/tr[1]/td[1]/a/@title")[0]       
-    except:
-        # if we're taken to the product page
-        title = dom.xpath("//*[@id='bookTitle']")[0]
-        title = title.text       
-    else:
-        return ["book unavailable", "https://www.goodreads.com"]
-    
-    # Find url
-    try:
-        # If we're taken to the search page
-        url = dom.xpath("//table/tr[1]/td[1]/a/@href")[0]
-        url = "https://www.goodreads.com" + url        
-    except:
-        # If we're taken to the product page
-        url = goodreadsURL
-    else:
-        return [title, "https://www.goodreads.com"]
-    
+        found = dom.xpath("//*[@id='searchresults']/li[1]/div[2]/h2/a")[0]
+    except IndexError:
+        return ["book unavailable", "", ""]
+
+    url = "https://isbnsearch.org" + found.get("href")
+
     # Find author
-    try: 
+    try:
         # If we're taken to the search or product page
-        author = dom.xpath("//*[@class='authorName']/span")[0]
-    except:
-        return [title, url, ""]
-    
-    
-    return [title, url, author.text]
+        author = dom.xpath("//*[@id='searchresults']/li[1]/div[2]/p[1]")[0]
+    except IndexError:
+        return [found.text, url, ""]
+    return [found.text, url, author.text]
